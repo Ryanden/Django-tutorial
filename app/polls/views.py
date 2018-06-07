9810f7bac3fbd99c1fb2b150adcfa1912e286c6b
Ryanden <lockstom@gmail.com>
2018-06-07 15:35:32 +0900

    index, detail, resuts, vote view를 url과 연결

--- app/polls/views.py
+++ app/polls/views.py
@@ -2,10 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from polls.models import Question
 
 
 def index(request):
-    return HttpResponse('hi')
+
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
