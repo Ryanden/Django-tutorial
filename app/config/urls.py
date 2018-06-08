3f77038b5fd8efef7cb8598a83da3195bb3eac8f
Ryanden <lockstom@gmail.com>
2018-06-08 11:34:02 +0900

    blog, polls에서 각각 index url name사용, namespace로 구분없이 사용

--- app/config/urls.py
+++ app/config/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.urls import path
+from common.views import index
 
 urlpatterns = [
    path('admin/', admin.site.urls),
-   path('polls/', include('polls.urls'))
+   path('polls/', include('polls.urls')),
+   path('blog/', include('blog.urls')),
+   path('', index)
 ]
